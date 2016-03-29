990192796fdadf976a492ec7b0ca587c4ca3ec9b
gertk <gertkommer@hotmail.com>
2016-03-29 23:05:37 +0200

    InitialCommit

---
+++ 1.1 - CamelCase/MyTextUtil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextUtil
+{
+    class MyTextUtil
+    {
+        public static String CamelCase(String s)
+        {
+            String[] words = s.Split(' ');
+            String returnString = "";
+            foreach(String s2 in words){
+                returnString += (char.ToUpper(s2[0]) + s2.Substring(1));
+            }
+            return returnString;
+        }
+
+        public static String UnCamelCase(String s)
+        {
+            String returnString = "";
+            foreach(char c in s){
+                if(char.IsUpper(c)){
+                    returnString += ((returnString.Length == 0) ? "": " ") + char.ToLower(c);
+                }else{
+                    returnString += c;
+                }
+            }
+            return returnString;
+        }
+    }
+}
---
+++ 1.1 - CamelCase/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextUtil;
+
+namespace CamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String normal = "camel cased string";
+            String camelCased = MyTextUtil.CamelCase(normal);
+            Console.WriteLine(camelCased);
+            String unCamelCased = MyTextUtil.UnCamelCase(camelCased);
+            Console.WriteLine(unCamelCased);
+            Console.Read();
+        }
+    }
+}
